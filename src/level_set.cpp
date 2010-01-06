211ef653857121297f5d6d275ef6e312ea44119e
Carl Reinke <carlreinke@users.noreply.github.com>
2010-01-06 14:56:51 -0600

    simplified congrats screen

--- src/level_set.cpp
+++ src/level_set.cpp
@@ -36,7 +36,8 @@ void play_pack( SDL_Surface *surface, const string &directory )
 			
 			if (!meta.good())
 			{
-				pack_done_screen(surface, pack_name);
+				CongratsLoop congrats(pack_name);
+				congrats.loop(surface);
 				return;
 			}
 			
@@ -74,126 +75,83 @@ void play_pack( SDL_Surface *surface, const string &directory )
 	}
 }
 
-void pack_done_screen( SDL_Surface *surface, const string &pack_name )
+CongratsLoop::CongratsLoop( const std::string &pack_name )
+: pack_name(pack_name)
 {
-	Fader fader(20);
-	fader.fade(Fader::in);
-	
-	std::list<Particle> particles;
-	
-	bool done = false;
-	while (!done && !global_quit)
+	// nothing to do
+}
+
+void CongratsLoop::handle_event( SDL_Event &e )
+{
+	switch (e.type)
 	{
-		SDL_WaitEvent(NULL);
-		
-		int updates = 0, frames = 0;
-		
-		SDL_Event e;
-		while (SDL_PollEvent(&e))
+	case SDL_KEYDOWN:
+		switch (e.key.keysym.sym)
 		{
-			switch (e.type)
-			{
-			case SDL_QUIT:
-				global_quit = true;
-				break;
-				
-			case SDL_KEYDOWN:
-				switch (e.key.keysym.sym)
-				{
-				case Controller::back_key:
-				case Controller::quit_key:
-				case Controller::select_key:
-				case Controller::start_key:
-					fader.fade(Fader::out);
-					break;
-					
-				case Controller::vol_up_key:
-					trigger_volume_change(0.1f);
-					break;
-				case Controller::vol_down_key:
-					trigger_volume_change(-0.1f);
-					break;
-					
-				default:
-					break;
-				}
-				break;
-				
-			case SDL_USEREVENT:
-				switch (e.user.code)
-				{
-				case USER_UPDATE:
-					++updates;
-					break;
-				case USER_FRAME:
-					++frames;
-					break;
-				}
-			}
+		case Controller::back_key:
+		case Controller::quit_key:
+		case Controller::select_key:
+		case Controller::start_key:
+			loop_quit = true;
+			break;
+			
+		default:
+			break;
 		}
 		
-		while (updates--)
-		{
-			for (vector<Controller *>::iterator c = controllers.begin(); c != controllers.end(); ++c)
-				(*c)->update();
-			
-			// random fireworks
-			if (rand() % 20 == 0)
-			{
-				const int x = rand() % (screen_width - screen_width / 4) + (screen_width / 8),
-				          y = rand() % (screen_height - screen_height / 3) + (screen_height / 8);
-				
-				const SDL_Color colors[] =
-				{
-					SDL_Color_RGBA(224, 64, 64),   // red
-					SDL_Color_RGBA(224, 224, 64),  // yellow
-					SDL_Color_RGBA(64, 224, 64),   // green
-					SDL_Color_RGBA(64, 64, 224),   // blue
-					SDL_Color_RGBA(224, 64, 224),  // violet
-					SDL_Color_RGBA(255, 255, 255), // white
-				};
-				const SDL_Color &color = colors[(x + y) % COUNTOF(colors)];
-				
-				for (uint i = 0; i < 40; ++i)
-				{
-					const Fixed radius = (Fixed(rand() % (1024 * 2) - 1024) / 1024),
-					            angle = (Fixed(rand() % 31415) / 10000),
-					            x_vel = cosf(angle) * radius,
-					            y_vel = sinf(angle) * radius;
-					
-					particles.push_back(FireworkParticle(x, y, x_vel, y_vel, color));
-				}
-				
-				play_sample(samples["nuke"], Fixed(1) / 5, Fixed(x) / screen_width);
-			}
-			
-			Particle::tick_all(particles);
-			
-			fader.update();
-			done = fader.is_done() && fader.was_fading(Fader::out);
-			
-			update_volume_notification();
-		}
-		
-		if (frames--)
-		{
-			SDL_FillRect(surface, NULL, 0);
-			
-			for (list<Particle>::const_iterator i = particles.begin(); i != particles.end(); ++i)
-				i->draw(surface, 0, 0, fader.value());
-			
-			font.blit(surface, surface->w / 2, surface->h / 4, "Congratulations!", font_sprites[3], Font::majuscule, Font::center, fader.value());
-			font.blit(surface, surface->w / 2, surface->h / 2, pack_name, font_sprites[4], Font::center, fader.value());
-			font.blit(surface, surface->w / 2, surface->h / 2 + font.height(font_sprites[4]), "completed!", font_sprites[3], Font::majuscule, Font::center, fader.value());
-			
-			draw_volume_notification(surface);
-			
-			SDL_Flip(surface);
-			
-			if (frames > 0)
-				clog << "dropped " << frames << " frame(s)" << endl;
-		}
+	default:
+		break;
 	}
+}
+
+void CongratsLoop::update( void )
+{
+	for (vector<Controller *>::iterator c = controllers.begin(); c != controllers.end(); ++c)
+		(*c)->update();
+	
+	// random fireworks
+	if (rand() % 20 == 0)
+	{
+		const int x = rand() % (screen_width - screen_width / 4) + (screen_width / 8),
+		          y = rand() % (screen_height - screen_height / 3) + (screen_height / 8);
+		
+		const SDL_Color colors[] =
+		{
+			SDL_Color_RGBA(224, 64, 64),   // red
+			SDL_Color_RGBA(224, 224, 64),  // yellow
+			SDL_Color_RGBA(64, 224, 64),   // green
+			SDL_Color_RGBA(64, 64, 224),   // blue
+			SDL_Color_RGBA(224, 64, 224),  // violet
+			SDL_Color_RGBA(255, 255, 255), // white
+		};
+		const SDL_Color &color = colors[(x + y) % COUNTOF(colors)];
+		
+		for (uint i = 0; i < 40; ++i)
+		{
+			const Fixed radius = (Fixed(rand() % (1024 * 2) - 1024) / 1024),
+			            angle = (Fixed(rand() % 31415) / 10000),
+			            x_vel = cosf(angle) * radius,
+			            y_vel = sinf(angle) * radius;
+			
+			particles.push_back(FireworkParticle(x, y, x_vel, y_vel, color));
+		}
+		
+		play_sample(samples["nuke"], Fixed(1) / 5, Fixed(x) / screen_width);
+	}
+	
+	Particle::tick_all(particles);
+}
+
+void CongratsLoop::draw( SDL_Surface *surface, Uint8 alpha ) const
+{
+	SDL_FillRect(surface, NULL, 0);
+	
+	for (list<Particle>::const_iterator i = particles.begin(); i != particles.end(); ++i)
+		i->draw(surface, 0, 0, alpha);
+	
+	font.blit(surface, surface->w / 2, surface->h / 4, "Congratulations!", font_sprites[3], Font::majuscule, Font::center, alpha);
+	font.blit(surface, surface->w / 2, surface->h / 2, pack_name, font_sprites[4], Font::center, alpha);
+	font.blit(surface, surface->w / 2, surface->h / 2 + font.height(font_sprites[4]), "completed!", font_sprites[3], Font::majuscule, Font::center, alpha);
 }
 
 void level_screen( SDL_Surface *surface, const Level &level, const Highscore &highscore )
--- src/level_set.hpp
+++ src/level_set.hpp
@@ -2,10 +2,25 @@
 #define LEVEL_SET_HPP
 
 #include "level.hpp"
-#include "SDL.h"
+#include "loop.hpp"
+#include "particle.hpp"
+
+class CongratsLoop : public Loop
+{
+public:
+	CongratsLoop( const std::string &pack_name );
+	
+	void handle_event( SDL_Event & );
+	void update( void );
+	void draw( SDL_Surface *, Uint8 alpha = SDL_ALPHA_OPAQUE ) const;
+	
+private:
+	const std::string pack_name;
+	
+	std::list<Particle> particles;
+};
 
 void play_pack( SDL_Surface *surface, const std::string &directory );
-void pack_done_screen( SDL_Surface *surface, const std::string &pack_name );
 
 void level_screen( SDL_Surface *surface, const Level &level, const Highscore &highscore );
 
